6b0d308c840a7faf56a51dc5d7fa749769a17dd1
mcshakes <efmccarthy26@gmail.com>
2019-04-23 17:09:23 -0600

    passing specs

---
+++ spec/requests/create_team_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Team Creation", :type => :request do
+	
+	it "creates a Team" do
+		headers = { "CONTENT_TYPE" => "application/json" }
+		post "/teams", :params => '{ "team": { "name": "My SuperTeam", "email": "SuperTeam@gmail.com", "password": "password123" } }', :headers => headers
+		
+		expect(response.content_type).to eq("application/json")
+	    expect(response).to have_http_status(:created)
+	    expect(response.status).to eq(201)
+	    expect(response).to be_successful
+	end
+end
