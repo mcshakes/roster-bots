f13db257ac042feb96cab8afffdd06154942e0b4
mcshakes <efmccarthy26@gmail.com>
2019-04-22 21:26:46 -0600

    Some extra validation tests. Will stop here to flesh out some more user stories, and probably bot models next

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
   subject { described_class.new(name: "Red Team", email: "redteam_rulez@gmail.com") }
 
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
   it "is valid with valid attributes" do
   	expect(subject).to be_valid
   end
@@ -15,6 +18,15 @@ RSpec.describe Team, type: :model do
   it "is invalid without an existing email" do
   	team_bad = Team.new(name: "poop", email: "")
   	expect(Team.new).to_not be_valid
+  end
+
+  it "in invalid with terribly formatted emails" do
+  	bad_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+
+  	bad_addresses.each do |add|
+  		subject.email = add
+  		expect(subject).to_not be_valid	
+  	end
   end
 
 end
